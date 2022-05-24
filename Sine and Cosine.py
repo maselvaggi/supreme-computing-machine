9fd91081f601542ba697b0458641e5b432d83639
Matt Selvaggi <mselvagg@stevens.edu>
2022-05-23 20:16:00 -0400

    Comments

--- Sine and Cosine.py
+++ Sine and Cosine.py
@@ -8,11 +8,11 @@ arr = np.arange(0,2*np.pi, 0.1)
 x = np.sin(arr)
 y = np.cos(arr)
 
-#plot and show the sine/cosine graphs
+#plot and show the sine & cosine graph with legend and title
 plt.plot(arr, x, label = "sine")
 plt.plot(arr, y, label = "cosine")
 plt.title("Sine and Cosine Graphs", fontsize = 20)
 plt.legend(loc = 'lower left')
 plt.show
 
-# %%
+
