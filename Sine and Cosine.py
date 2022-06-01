b47df1683e4bbcba1a63111007b0b53c3e9532f8
Matt Selvaggi <mselvagg@stevens.edu>
2022-06-01 13:17:45 -0400

    Inclusion of main

--- Sine and Cosine.py
+++ Sine and Cosine.py
@@ -1,19 +1,29 @@
 
 #%%
+from cmath import sin
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 #%%
-arr = np.arange(0,2*np.pi, 0.1)
+# x - start of range
+# y - end of range
+# z - frequency of points plotted
+def sincos(x,y,z):
+    arr = np.arange(x,y*np.pi, z)
 
-x = np.sin(arr)
-y = np.cos(arr)
+    x = np.sin(arr)
+    y = np.cos(arr)
 
-#plot and show the sine & cosine graph with legend and title
-plt.plot(arr, x, label = "sine")
-plt.plot(arr, y, label = "cosine")
-plt.title("Sine and Cosine Graphs", fontsize = 20)
-plt.legend(loc = 'lower left')
-plt.show
+    #plot and show the sine & cosine graph with legend and title
+    plt.plot(arr, x, label = "sine")
+    plt.plot(arr, y, label = "cosine")
+    plt.title("Sine and Cosine Graphs", fontsize = 20)
+    plt.legend(loc = 'lower left')
+    plt.show
 
+# %%
+if __name__ == "__main__":
+    #sincos(0,2,0.1)
+    #sincos(10,5,0.5)
+    sincos(-5,3,1)       
